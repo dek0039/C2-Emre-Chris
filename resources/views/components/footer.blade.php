239c771a7d5e7e7b09f557cf46580791de185c1b
dek0039 <154227154+dek0039@users.noreply.github.com>
2024-10-09 08:58:05 +0200

    Add sections to footer

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,28 +1,42 @@
-
 <footer>
-	© {{ __('misc.copyright') }}
+  <div class="footer-element">
+  <p><b>Contact</b></p>
+  <p>Telefoon: </p>
+  <p>Email: </p>
+  </div>
+  <div class="footer-element">
+    <p><b>Social</b></p>
+    <a>Instagram</a>
+    <a>Facbook</a>
+  </div>
+  <div class="footer-element">
+    <p><b>Over Ons</b></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores provident magnam aliquid rerum commodi, quibusdam.</p>
+  </div>
+
+  <p><b>© {{ __('misc.copyright') }}</b></p>
 </footer>
 
 
-<!-- analytics code -->              
+<!-- analytics code -->
 <script type="text/javascript">
-
   var _gaq = _gaq || [];
   _gaq.push(['_setAccount', 'UA-30506707-1']);
   _gaq.push(['_trackPageview']);
 
   (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    var ga = document.createElement('script');
+    ga.type = 'text/javascript';
+    ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(ga, s);
   })();
 </script>
 <!-- Einde analytics code -->
 
-<script language="Javascript" type="text/javascript"> 
- 
- if (top.location!= self.location) { 
-  top.location = self.location.href
- } 
- 
+<script language="Javascript" type="text/javascript">
+  if (top.location != self.location) {
+    top.location = self.location.href
+  }
 </script>
